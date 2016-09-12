fa2accda0dbb9789ca6f12b425149860ed12d7f3
Chris Van Patten <chris@vanpattenmedia.com>
2016-09-12 10:26:40 -0400

    allow empty strings for none

--- src/Pluralize.php
+++ src/Pluralize.php
@@ -34,20 +34,25 @@ class Pluralize extends Twig_Extension
 	/**
 	 * Get a plural variation of a string
 	 *
-	 * @param  integer $count The count that forms the basis of the pluralizer. Passed into sprintf
-	 * @param  string  $one   The string to display when the count is 1. Use sprintf syntax, if desired
-	 * @param  string  $many  The string to display when the count is not 1 or 0. Use sprintf syntax, if desired
-	 * @param  string  $none  The string to display when the count is 0. Use sprintf syntax, if desired
+	 * @param  integer      $count The count that forms the basis of the
+	 *                               pluralizer. Passed into sprintf
+	 * @param  string       $one   The string to display when the count
+	 *                               is 1. Use sprintf syntax, if desired
+	 * @param  string       $many  The string to display when the count
+	 *                               is not 1 or 0. Use sprintf syntax,
+	 *                               if desired
+	 * @param  string|null  $none  The string to display when the count
+	 *                               is 0. Use sprintf syntax, if desired
 	 * @return string
 	 */
-	public function getPluralizedString( $count, $one, $many, $none = '' )
+	public function getPluralizedString( $count, $one, $many, $none = null )
 	{
 		// Make sure $count is a numeric value
 		if ( ! is_numeric( $count ) )
 			throw new Exception( '$count must be numeric.' );
 
-		// If the option for $none isn't set, use the option for $many
-		if ( empty( $none ) )
+		// If the option for $none is null, use the option for $many
+		if ( $none === null )
 			$none = $many;
 
 		// Handle 0
