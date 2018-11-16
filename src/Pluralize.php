aa9f41e621ef22f9b5558b7b16e1a5136eb2e2e0
Chris Van Patten <chris@vanpattenmedia.com>
2018-11-16 08:47:49 -0500

    Rework to use Tomodomo name and update code styles

--- src/Pluralize.php
+++ src/Pluralize.php
@@ -1,75 +1,75 @@
 <?php
 
-namespace VanPattenMedia\Twig;
-
-use Twig_Extension;
+namespace Tomodomo\Twig;
 
 use Exception;
+use Twig_Extension;
 
 class Pluralize extends Twig_Extension
 {
+    /**
+     * Returns the name of this extension
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return 'pluralize';
+    }
 
-	/**
-	 * Returns the name of this extension
-	 *
-	 * @return string
-	 */
-	public function getName()
-	{
-		return 'pluralize';
-	}
+    /**
+     * Returns the functions this extension adds
+     *
+     * @return array
+     */
+    public function getFunctions()
+    {
+        return [
+            new \Twig_SimpleFunction(
+                'pluralize',
+                [$this, 'getPluralizedString']
+            )
+        ];
+    }
 
-	/**
-	 * Returns the functions this extension adds
-	 *
-	 * @return array
-	 */
-	public function getFunctions()
-	{
-		return [
-			new \Twig_SimpleFunction( 'pluralize', [ $this, 'getPluralizedString' ] )
-		];
-	}
+    /**
+     * Get a plural variation of a string
+     *
+     * @param  integer      $count The count that forms the basis of the
+     *                               pluralizer. Passed into sprintf
+     * @param  string       $one   The string to display when the count
+     *                               is 1. Use sprintf syntax, if desired
+     * @param  string       $many  The string to display when the count
+     *                               is not 1 or 0. Use sprintf syntax,
+     *                               if desired
+     * @param  string|null  $none  The string to display when the count
+     *                               is 0. Use sprintf syntax, if desired
+     * @return string
+     */
+    public function getPluralizedString($count, $one, $many, $none = null)
+    {
+        // Make sure $count is a numeric value
+        if (!is_numeric($count)) {
+            throw new Exception('$count must be numeric.');
+        }
 
-	/**
-	 * Get a plural variation of a string
-	 *
-	 * @param  integer      $count The count that forms the basis of the
-	 *                               pluralizer. Passed into sprintf
-	 * @param  string       $one   The string to display when the count
-	 *                               is 1. Use sprintf syntax, if desired
-	 * @param  string       $many  The string to display when the count
-	 *                               is not 1 or 0. Use sprintf syntax,
-	 *                               if desired
-	 * @param  string|null  $none  The string to display when the count
-	 *                               is 0. Use sprintf syntax, if desired
-	 * @return string
-	 */
-	public function getPluralizedString( $count, $one, $many, $none = null )
-	{
-		// Make sure $count is a numeric value
-		if ( ! is_numeric( $count ) )
-			throw new Exception( '$count must be numeric.' );
+        // If the option for $none is null, use the option for $many
+        $none = $none ?? $many;
 
-		// If the option for $none is null, use the option for $many
-		if ( $none === null )
-			$none = $many;
+        // Choose the correct string
+        switch($count) {
+            case 0:
+                $string = $none;
+                break;
+            case 1:
+                $string = $one;
+                break;
+            default:
+                $string = $many;
+                break;
+        }
 
-		// Handle 0
-		switch( $count ) {
-			case 0:
-				$string = $none;
-				break;
-			case 1:
-				$string = $one;
-				break;
-			default:
-				$string = $many;
-				break;
-		}
-
-		// Return the result
-		return sprintf( $string, $count );
-	}
-
+        // Return the result
+        return sprintf($string, $count);
+    }
 }
